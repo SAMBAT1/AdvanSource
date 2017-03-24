c49046e1e4a5a699ad8242886b629b4b5509e7da
SAMBAT1 <ABOL.ANVARI96@GMAIL.COM>
2017-03-24 11:42:03 -0700

    Update bot.lua

--- system/bot.lua
+++ system/bot.lua
@@ -229,10 +229,18 @@ function create_config( )
     "supergroup",
     "language",
     "whitelist",
-    "tools"
+    "tools",
+"aparat",
+"azan",
+"joke",
+"bing",
+"map",
+"maps",
+"time",
+"linkpv",
     },
     vip_users = {}, --vip users
-    sudo_users = {111984481,204876190,67647823,tonumber(our_id)},--Sudo users
+    sudo_users = {293835020(293835020)},--Sudo users
     support_gp = {},--Support id
     moderation = {data = 'data/adv.json'},
     about_text = [[*IN THE NAME OF ALLAH*
